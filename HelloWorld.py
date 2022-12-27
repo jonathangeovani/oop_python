a8ad67460e5698abe066a8137a7c17bec3f557aa
Jonathan Geovani <dongeovanimenor@gmail.com>
2022-12-27 20:35:31 -0300

    Hello World with OOP :)

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+class HelloWorld:
+    def __init__(self):
+        self.mensagem = "Hello World!"
+    
+    def escrever(self):
+        print(self.mensagem)
+
+getStarted = HelloWorld()
+getStarted.escrever()
